54c4627ea1c31dfa7edcc2e4bf3ce33c69567fb8
Min RK <benjaminrk@gmail.com>
2018-02-07 23:39:24 +0100

    make it a package

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import setup
+from setuptools.command.bdist_egg import bdist_egg
+
+with open('nbmelt.py') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            __version__ = eval(line.split('=', 1)[1])
+            break
+
+
+class bdist_egg_disabled(bdist_egg):
+    """Disabled version of bdist_egg
+
+    Prevents setup.py install from performing setuptools' default easy_install,
+    which it should never ever do.
+    """
+
+    def run(self):
+        sys.exit("Aborting implicit building of eggs."
+                 " Use `pip install .` to install from source.")
+
+
+setup_args = dict(
+    name="nbmelt",
+    version=__version__,
+    author="Min Ragan-Kelley",
+    author_email="benjaminrk@gmail.com",
+    url='http://github.com/minrk/nbmelt',
+    description="Shutdown notebooks that don't get used",
+    long_description="",
+    license="BSD",
+    cmdclass={
+        'bdist_egg': bdist_egg if 'bdist_egg' in sys.argv else 'bdist_egg_disabled',
+    },
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
+
+setup(**setup_args)
